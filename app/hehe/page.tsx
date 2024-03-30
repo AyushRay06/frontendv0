247ad61ae68be8347f6ff207375791e304dbd084
AyushRay06 <rayayush47@gmail.com>
2024-03-31 03:15:54 +0530

    Prject-setup

---
+++ app/hehe/page.tsx
@@ -0,0 +1,11 @@
+import { Product } from "@/components/product"
+import { Signup } from "@/components/signup"
+
+export default function SignupPage() {
+  return (
+    <>
+      <Signup />
+      <Product />
+    </>
+  )
+}
